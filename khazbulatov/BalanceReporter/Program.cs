2cc24f08c172757bc710634e486ab4b2350392ff
Arthur Khazbulatov <atkhazbulatov@edu.hse.ru>
2020-09-10 02:05:42 +0300

    Add new solution, projects, .gitignore

---
+++ khazbulatov/BalanceReporter/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BalanceReporter
+{
+    static class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ khazbulatov/BalanceReporterTests/Tests.cs
@@ -0,0 +1,14 @@
+using NUnit.Framework;
+
+namespace BalanceReporterTests
+{
+    [TestFixture]
+    public class BalanceReporterTests
+    {
+        [Test]
+        public void Test()
+        {
+            Assert.Pass();
+        }
+    }
+}
