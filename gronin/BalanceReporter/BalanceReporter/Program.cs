154e59db38515ec0072f423e03039406131ac50f
AndrewGronin <you@example.com>
2020-09-10 17:58:04 +0300

    Added console program for usage

--- gronin/BalanceReporter/BalanceReporter/Program.cs
+++ gronin/BalanceReporter/BalanceReporter/Program.cs
@@ -8,30 +8,22 @@ namespace BalanceReporter
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Insert starting date of the period dd-mm-yyyy");
+            var from = DateTime.Parse(Console.ReadLine());
+            Console.WriteLine("Insert ending date of the period dd-mm-yyyy");
+            var to = DateTime.Parse(Console.ReadLine());
+            
             
             string path = Directory.GetParent(Environment.CurrentDirectory).Parent?.Parent?.FullName + "\\myFile0.csv";
             Accounter account = new Accounter(path);
-            var res = account.TransactionsPerMonth(2019, 3);
-            Console.WriteLine("Monthly transactions");
-            foreach (var tr in res)
-            {
-                Console.WriteLine("date: {0} Company: {1}, Sum: {2}", tr.Date, tr.Sender, tr.Amount);
-            }
-            
-            var res2 = account.TransactionsPerYear(2020);
-            Console.WriteLine("Yearly transactions");
-            foreach (var tr in res2)
-            {
-                Console.WriteLine("date: {0} Company: {1}, Sum: {2}", tr.Date, tr.Sender, tr.Amount);
-            }
-            Console.WriteLine(account.AverageIncome(new DateTime(2019, 3, 1), new DateTime(2019,5,31)));
-            Console.WriteLine(account.AverageExpense(new DateTime(2019, 3, 1), new DateTime(2019,5,31)));
-            Console.WriteLine(account.MaxIncome(new DateTime(2019, 3, 1), new DateTime(2019,5,31)));
-            Console.WriteLine(account.MaxExpense(new DateTime(2019, 3, 1), new DateTime(2019,5,31)));
-            Console.WriteLine(account.MaxMoneySender(new DateTime(2019, 3, 1), new DateTime(2019,5,31)));
-            Console.WriteLine(account.MaxMoneyReceiver(new DateTime(2019, 3, 1), new DateTime(2019,5,31)));
-            Console.WriteLine(account.TotalIncome(new DateTime(2021, 1, 1), new DateTime(2021,5,31)));
-            Console.WriteLine(account.TotalExpense(new DateTime(2019, 3, 1), new DateTime(2019,5,31)));
+            Console.WriteLine("AverageIncome: " + account.AverageIncome(from,to));
+            Console.WriteLine("AverageExpense: " + account.AverageExpense(from,to));
+            Console.WriteLine("MaxIncome: " + account.MaxIncome(from,to));
+            Console.WriteLine("MaxExpense: " + account.MaxExpense(from,to));
+            Console.WriteLine("MaxMoneySender: " + account.MaxMoneySender(from,to));
+            Console.WriteLine("MaxMoneyReceiver: " + account.MaxMoneyReceiver(from,to));
+            Console.WriteLine("TotalIncome: " + account.TotalIncome(from,to));
+            Console.WriteLine("TotalExpense: " + account.TotalExpense(from,to));
             
         }
     }
