522fee3501452e7f4c4e46cf618a6767b468ae30
AndrewGronin <you@example.com>
2020-09-10 18:12:33 +0300

    commit to test fix off rider

--- gronin/BalanceReporter/BalanceReporter/Program.cs
+++ gronin/BalanceReporter/BalanceReporter/Program.cs
@@ -12,19 +12,17 @@ namespace BalanceReporter
             var from = DateTime.Parse(Console.ReadLine());
             Console.WriteLine("Insert ending date of the period dd-mm-yyyy");
             var to = DateTime.Parse(Console.ReadLine());
-            
-            
+
             string path = Directory.GetParent(Environment.CurrentDirectory).Parent?.Parent?.FullName + "\\myFile0.csv";
             Accounter account = new Accounter(path);
-            Console.WriteLine("AverageIncome: " + account.AverageIncome(from,to));
-            Console.WriteLine("AverageExpense: " + account.AverageExpense(from,to));
-            Console.WriteLine("MaxIncome: " + account.MaxIncome(from,to));
-            Console.WriteLine("MaxExpense: " + account.MaxExpense(from,to));
-            Console.WriteLine("MaxMoneySender: " + account.MaxMoneySender(from,to));
-            Console.WriteLine("MaxMoneyReceiver: " + account.MaxMoneyReceiver(from,to));
-            Console.WriteLine("TotalIncome: " + account.TotalIncome(from,to));
-            Console.WriteLine("TotalExpense: " + account.TotalExpense(from,to));
-            
+            Console.WriteLine("AverageIncome: " + account.AverageIncome(from, to));
+            Console.WriteLine("AverageExpense: " + account.AverageExpense(from, to));
+            Console.WriteLine("MaxIncome: " + account.MaxIncome(from, to));
+            Console.WriteLine("MaxExpense: " + account.MaxExpense(from, to));
+            Console.WriteLine("MaxMoneySender: " + account.MaxMoneySender(from, to));
+            Console.WriteLine("MaxMoneyReceiver: " + account.MaxMoneyReceiver(from, to));
+            Console.WriteLine("TotalIncome: " + account.TotalIncome(from, to));
+            Console.WriteLine("TotalExpense: " + account.TotalExpense(from, to));
         }
     }
 }
