21c565c2f3580f1876449c3d0955095f77b2c714
moskovets <n.moskovets@yandex.ru>
2020-09-11 22:45:15 +0300

    add class for base balace calculation and reading from csv

---
+++ moskovets/BalanceReporter/Balance.cs
@@ -0,0 +1,85 @@
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using CsvHelper;
+using CsvHelper.Configuration.Attributes;
+
+namespace BalanceReporter
+{
+    public enum TransactionType
+    {
+        Income,
+        Expense
+    }
+
+    public class Balance
+    {
+        public class Transaction
+        {
+            [Index(0)] public DateTime Date { get; set; }
+            [Index(1)] public string Receiver { get; set; }
+            [Index(2)] public double Total { get; set; }
+        }
+
+        private List<Transaction> _transactions;
+
+        private bool doesTransactionTypeMatch(Transaction transaction, TransactionType transactionType)
+        {
+            if (transactionType == TransactionType.Income)
+            {
+                return transaction.Total >= 0;
+            }
+
+            return transaction.Total < 0;
+        }
+
+        public void ReadTransactionsFromCsvFile(string filename)
+        {
+            using (StreamReader streamReader = new StreamReader(filename))
+            {
+                using (CsvReader csvReader = new CsvReader(streamReader, CultureInfo.InvariantCulture))
+                {
+                    csvReader.Configuration.Delimiter = ",";
+                    csvReader.Configuration.HasHeaderRecord = false;
+                    IEnumerable<Transaction> records = csvReader.GetRecords<Transaction>();
+                    _transactions = records.ToList();
+                }
+            }
+        }
+
+        public double CalculateTotalForThePeriod(DateTime from, DateTime to, TransactionType transactionType)
+        {
+            double total = 0;
+            foreach (var transaction in _transactions)
+            {
+                if (doesTransactionTypeMatch(transaction, transactionType)
+                    && transaction.Date >= from && transaction.Date <= to)
+                {
+                    total += transaction.Total;
+                }
+            }
+
+            return Math.Abs(total);
+        }
+
+
+        public string FindReceiverWithMaxTransactionSums(DateTime from, DateTime to, TransactionType transactionType)
+        {
+            var receiversIncomes = _transactions
+                .Where(transaction => doesTransactionTypeMatch(transaction, transactionType) &&
+                                      transaction.Date >= from &&
+                                      transaction.Date <= to)
+                .GroupBy(transaction => transaction.Receiver)
+                .Select(receiver => new
+                {
+                    Name = receiver.Key,
+                    Sum = receiver.Sum(transaction => Math.Abs(transaction.Total))
+                });
+            var maxIncome = receiversIncomes.Max(x => x.Sum);
+            var result = receiversIncomes.First(x => x.Sum == maxIncome);
+            return result.Name;
+        }
+    }
+}
---
+++ moskovets/BalanceReporter/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace BalanceReporter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Balance balance = new Balance();
+            balance.ReadTransactionsFromCsvFile(@"D:\MINDBOX\balance1.txt");
+            DateTime from = new DateTime(2019, 1, 1);
+            DateTime to = new DateTime(2020, 1, 1);
+            Console.WriteLine(balance.CalculateTotalForThePeriod(from, to, TransactionType.Income));
+            Console.WriteLine(balance.CalculateTotalForThePeriod(from, to, TransactionType.Expense));
+            Console.WriteLine(balance.FindReceiverWithMaxTransactionSums(from, to, TransactionType.Income));
+        }
+    }
+}
