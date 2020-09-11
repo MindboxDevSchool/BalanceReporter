59285e0f85a54922573493b40015e2645a26f768
iliazen <iliazenin.msu@gmail.com>
2020-09-11 10:19:57 +0300

    Counting earnings and costs added

--- zenin/BalanceReporter/Program.cs
+++ zenin/BalanceReporter/Program.cs
@@ -3,18 +3,21 @@ using System.Collections.Generic;
 using System.Security.Permissions;
 using System.IO;
 using System.Linq;
+using System.Data;
 
 namespace BalanceReporter
 {
     public class Program
     {
-        public static int FromCSVtoLists(string fileName)
+        public static DataTable FromCSVtoDataTable(string fileName)
         {
-            List<string> dateList = new List<string>();
-            List<string> nameList = new List<string>();
-            List<double> moneyList = new List<double>();
             //string path = System.IO.Directory.GetCurrentDirectory();
             var path = @"C:\Users\ilyaz\RiderProjects\BalanceReporter\zenin\BalanceReporter\";
+            
+            DataTable table = new DataTable();
+            table.Columns.Add("date", typeof(string));
+            table.Columns.Add("name", typeof(string));
+            table.Columns.Add("amount", typeof(double));
             
             using (var reader = new StreamReader(path + fileName))
             {
@@ -22,61 +25,127 @@ namespace BalanceReporter
                 {
                     var line = reader.ReadLine();
                     var values = line.Split(';');
-
-                    dateList.Add(values[0]);
-                    nameList.Add(values[1]);
-                    moneyList.Add(Convert.ToDouble((values[2])));
+                    table.Rows.Add(values[0], values[1], Convert.ToDouble((values[2])));
                 }
             }
 
-            MonthsBalance(dateList, nameList, moneyList);
-            return dateList.Capacity;
+            return table;
         }
-
-        public static int MonthsBalance(List<string> dateList, List<string> nameList, List<double> moneyList)
+        
+        public static int FindIndexOfStringInColumn(DataTable table, string value, string column)
         {
-            List<string> months = new List<string>();
-            List<double> earned = new List<double>();
-            List<double> spent = new List<double>();
-
-            foreach (string record in dateList)
+            int index = -1;
+            foreach (DataRow balanceRow in table.Rows)
             {
-                var month = record.Split('.')[1] + " " + record.Split('.')[2];
-                if (!months.Contains(month))
-                {
-                    //Console.WriteLine(month);
-                    months.Add(month);
-                    earned.Add(0.0);
-                    spent.Add(0.0);
-                }
-
-                var index = months.IndexOf(month);
-                //Console.WriteLine(index);
-                var money = moneyList[dateList.IndexOf(record)];
-                if (money >= 0)
-                    earned[index] += money;
-                else
-                    spent[index] += money;
+                if (balanceRow[column].ToString() == value)
+                    index = table.Rows.IndexOf(balanceRow);
             }
 
-            PrintBalance(months, earned, spent);
-            return months.Capacity;
+            return index;
         }
 
-        public static void PrintBalance(List<string> periods, List<double> earned, List<double> spent)
+        public static DataTable CreateMovementTable()
         {
-            foreach (string period in periods)
+            DataTable movementTable = new DataTable();
+            movementTable.Columns.Add("period", typeof(string));
+            movementTable.Columns.Add("earned", typeof(double));
+            movementTable.Columns.Add("spent", typeof(double));
+            return movementTable;
+        }
+
+        public static DataTable AddAmountInMovementTable(DataTable movementTable, string period, double amount)
+        {
+            int index = FindIndexOfStringInColumn(movementTable, period, "period");
+
+            if (index == -1)
             {
-                var index = periods.IndexOf(period);
-                Console.WriteLine("{0} {1} {2}", period, earned[index], spent[index]);
+                movementTable.Rows.Add(period, 0.0, 0.0);
+                index = FindIndexOfStringInColumn(movementTable, period, "period");
+            }
+
+            if (amount >= 0)
+            {
+                var earned = amount + (double) movementTable.Rows[index]["earned"];
+                movementTable.Rows[index].SetField(1, earned);
+            }
+            else
+            {
+                var spent = amount + (double) movementTable.Rows[index]["spent"];
+                movementTable.Rows[index].SetField(2, spent);
+            }
+
+            return movementTable;
+        }
+
+        public static DataTable CreateMonthsMovementsTable(DataTable dataTable)
+        {
+            string[] months = {"January","February","March","April","May","June",
+                "July", "August","September","October","November","December"};
+
+            DataTable movementTable = CreateMovementTable();
+
+            foreach (DataRow row in dataTable.Rows)
+            {
+                var month = row["date"].ToString();
+                month = months[Convert.ToInt32(month.Split('.')[1])-1] + " " + month.Split('.')[2];
+
+                movementTable = AddAmountInMovementTable(movementTable, month, (double)row["amount"]);
+            }
+
+            return movementTable;
+        }
+        
+        public static DataTable CreateYearsMovementsTable(DataTable monthsMovementTable)
+        {
+            DataTable yearsMovementTable = CreateMovementTable();
+            
+            foreach (DataRow row in monthsMovementTable.Rows)
+            {
+                var year = row["period"].ToString();
+                year = year.Split(' ')[1];
+                
+                yearsMovementTable = AddAmountInMovementTable(yearsMovementTable, year, (double) row["earned"]);
+                yearsMovementTable = AddAmountInMovementTable(yearsMovementTable, year, (double) row["spent"]);
+                
+            }
+            return yearsMovementTable;
+        }
+
+
+        public static void FindMeansByMonths(DataTable dataTable)
+        {
+            
+        }
+        
+        public static void FindMeansByYears(DataTable table)
+        {
+            
+        }
+        
+        public static void FindOverallMeans(DataTable table)
+        {
+            
+        }
+
+        public static void PrintMovements(DataTable table)
+        {
+            foreach(DataRow row in table.Rows)
+            {
+                Console.WriteLine(row["period"]);
+                Console.WriteLine("Earned: {0}", row["earned"]);
+                Console.WriteLine("Spent: {0}", (double)row["spent"]*-1);
             }
         }
         
         static void Main(string[] args)
         {
             string fileName = "data.csv";
-            FromCSVtoLists(fileName);
-            //Console.WriteLine(FromCSVtoLists(fileName));
+            DataTable dataTable = FromCSVtoDataTable(fileName);
+            DataTable monthsMovementTable = CreateMonthsMovementsTable(dataTable);
+            Console.WriteLine("Months movements:");
+            PrintMovements(monthsMovementTable);
+            Console.WriteLine("Years movements:");
+            PrintMovements(CreateYearsMovementsTable(monthsMovementTable));
         }
     }
 }
