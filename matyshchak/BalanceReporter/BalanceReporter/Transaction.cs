ec8faa2c53eb87ad1cfb6aad8fb72563d85c86c2
steppam <steppam@mail.ru>
2020-09-11 20:04:53 +0300

    Added positive test for CsvParser

--- matyshchak/BalanceReporter/BalanceReporter/Transaction.cs
+++ matyshchak/BalanceReporter/BalanceReporter/Transaction.cs
@@ -14,5 +14,20 @@ namespace BalanceReporter
         public DateTime Date { get; }
         public string TransactionPartner { get; }
         public double Amount { get; }
+
+        public override bool Equals(object? obj) =>
+            (obj is Transaction transaction) && Equals(transaction);
+
+        public bool Equals(Transaction other)
+        {
+            return Date.Equals(other.Date)
+                   && TransactionPartner == other.TransactionPartner
+                   && Amount.Equals(other.Amount);
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Date, TransactionPartner, Amount);
+        }
     }
 }
---
+++ matyshchak/BalanceReporter/BalanceReporterTests/TransactionsParserFromCsvTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using BalanceReporter;
+using NUnit.Framework;
+
+namespace BalanceReporterTests
+{
+    public class TransactionsParserFromCsvTests
+    {
+        [Test]
+        public void Parsing_file_in_expected_format_works_fine()
+        {
+            var directory = TestContext.CurrentContext.TestDirectory;
+            var filePath = System.IO.Path.Combine(directory, @"..\..\..\transactions_for_testing.csv");
+            var actualTransactions = TransactionsParserFromCsv.Parse(filePath);
+
+            var expectedTransactions = new List<Transaction>()
+            {
+                new Transaction(
+                    new DateTime(2020, 1, 1),
+                    "Vkusvill",
+                    -1)
+            };
+            
+            Assert.That(actualTransactions, Is.EqualTo(expectedTransactions));
+        }
+    }
+}
